205b1e4323317424cd448bcddc1263b3647a633e
SeungminKim <ksm0057@naver.com>
2020-12-21 21:30:12 +0900

    Cart page 2

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,27 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getCartItems } from "../../../_actions/user_actions";
 
 function CartPage(props) {
   const dispatch = useDispatch();
 
-  useState(() => {
-    const userData = props.user.userData;
-
-    let cartItem = [];
-
-    //Redux User state 안 Cart에 상품이 있는지 확인
-    if (userData && userData.cart) {
-      if (userData.cart.length > 0) {
-        userData.cart.forEach((item) => {
-          cartItem.push(item.id);
+  useEffect(() => {
+    let cartItems = [];
+    if (props.user.userData && props.user.userData.cart) {
+      if (props.user.userData.cart.length > 0) {
+        props.user.userData.cart.forEach((item) => {
+          cartItems.push(item.id);
+          console.log(item);
         });
-        dispatch(getCartItems(cartItem, userData.cart));
+        dispatch(getCartItems(cartItems, props.user.userData.cart));
       }
     }
   }, [props.user.userData]);
 
-  return <div></div>;
+  return <div>CartPage</div>;
 }
 
 export default CartPage;
