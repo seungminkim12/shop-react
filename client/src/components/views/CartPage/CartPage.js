aeb2603fdae38e845d707cf7d9c99530e0e91ef7
SeungminKim <ksm0057@naver.com>
2020-12-22 17:55:55 +0900

    Cart Page 3

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,24 +1,35 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getCartItems } from "../../../_actions/user_actions";
+import UserCardBlock from "./Sections/UserCardBlock";
 
 function CartPage(props) {
   const dispatch = useDispatch();
+  const userData = props.user.userData;
 
   useEffect(() => {
     let cartItems = [];
-    if (props.user.userData && props.user.userData.cart) {
-      if (props.user.userData.cart.length > 0) {
-        props.user.userData.cart.forEach((item) => {
+    if (userData && userData.cart) {
+      if (userData.cart.length > 0) {
+        userData.cart.forEach((item) => {
           cartItems.push(item.id);
           console.log(item);
         });
-        dispatch(getCartItems(cartItems, props.user.userData.cart));
+        dispatch(getCartItems(cartItems, userData.cart));
       }
     }
-  }, [props.user.userData]);
+  }, [userData]);
 
-  return <div>CartPage</div>;
+  return (
+    <div style={{ width: "85%", margin: "3rem auto" }}>
+      <h1>My cart</h1>
+      <div>
+        <UserCardBlock
+          products={props.user.cartDetail && props.user.cartDetail.product}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default CartPage;
