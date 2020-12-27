a27246a2de563662766835705439b60a3d0564a5
SeungminKim <ksm0057@naver.com>
2020-12-27 21:37:50 +0900

    Function

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import {Icon} from 'antd';
+import React from "react";
+import { Icon } from "antd";
 
 function Footer() {
-    return (
-        <div style={{
-            height: '80px', display: 'flex',
-            flexDirection: 'column', alignItems: 'center',
-            justifyContent: 'center', fontSize:'1rem'
-        }}>
-           <p> Happy Coding  <Icon type="smile" /></p>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        height: "80px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "1rem",
+      }}
+    >
+      <p>
+        &#169;Seungmin Kim <Icon type="smile" />
+      </p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
