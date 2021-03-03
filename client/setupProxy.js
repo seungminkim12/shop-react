188902b4fb0478aecadabc1b8216410c10ad8770
SeungminKim <ksm0057@naver.com>
2021-03-03 22:56:02 +0900

    Fix?

--- client/setupProxy.js
+++ client/setupProxy.js
@@ -2,7 +2,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
+    "/api",
+    createProxyMiddleware({
       target: "http://localhost:5000",
       changeOrigin: true,
     })
