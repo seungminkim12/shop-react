c38825c5dc933e3c2ee5bc57f19630482f4dd6b3
SeungminKim <ksm0057@naver.com>
2021-07-07 23:15:19 +0900

    Editing...7

--- client/src/components/utils/ImageSlider.js
+++ client/src/components/utils/ImageSlider.js
@@ -7,7 +7,7 @@ function ImageSlider(props) {
       <Carousel autoplay>
         {props.images.map((image, index) => (
           <div key={index}>
-            <img style={{ width: "100%", maxHeight: "150px" }} src={image} />
+            <img style={{ width: "100%", maxHeight: "110px" }} src={image} />
           </div>
         ))}
       </Carousel>
